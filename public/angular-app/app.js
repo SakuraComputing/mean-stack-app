0f9c1ddabd375d9115e0a4d2de4b8d4b2a758ddc
Ewan Bauld <ewan.bauld@gmail.com>
2018-03-31 16:07:37 +0100

    Add navigation bar, use correct bootstrap version

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -3,9 +3,12 @@ angular.module('meanhotel',['ngRoute']).config(config);
 function config($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'angular-app/hotel-list/hotels.html',
-            controller: HotelsController,
-            controllerAs: 'vm'
+            templateUrl: 'angular-app/main/main.html'
+    })
+    .when('/hotels', {
+        templteUrl: 'angular-app/hotel-list/hotels.html',
+        controller: HotelsController,
+        controllerAs: 'vm'
     })
     .when('/hotel/:id', {
         templateUrl: 'angular-app/hotel-display/hotel.html',
@@ -16,6 +19,9 @@ function config($routeProvider) {
         templateUrl: 'angular-app/register/register.html',
         controller: RegisterController,
         controllerAs: 'vm'
+    })
+    .otherwise({
+      redirectTo: '/'
     });
 }
 
